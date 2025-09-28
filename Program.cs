60d9f1b8fa91c5498e0cfdea7fd6e93cadc0573a
Iryna <kytsenkoiryna33@gmail.com>
2025-09-28 19:52:39 +0300

    ISP

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+interface ILightControl
+{
+    void SwitchLight(bool on);
+}
+
+interface IMusicControl
+{
+    void PlayMusic(string track);
+}
+
+class SmartLamp : ILightControl
+{
+    public void SwitchLight(bool on) =>
+        Console.WriteLine(on ? "Light ON" : "Light OFF");
+}
+
+class SmartSpeaker : IMusicControl
+{
+    public void PlayMusic(string track) =>
+        Console.WriteLine($"Playing: {track}");
+}
+
+class Program
+{
+    static void Main()
+    {
+        ILightControl lamp = new SmartLamp();
+        IMusicControl speaker = new SmartSpeaker();
+
+        lamp.SwitchLight(true);
+        speaker.PlayMusic("Imagine - John Lennon");
+    }
+}
+
